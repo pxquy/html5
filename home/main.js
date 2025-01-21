ea2fcdca2b29749a1c58b8dae553b6ea3acf34d1
pvq2k2 <vanquyet2392002@gmail.com>
2025-01-21 14:47:36 +0700

    aside nav - xử lý khéo trang giữ nav và header

--- home/main.js
+++ home/main.js
@@ -10,3 +10,26 @@ $(".slide-deal-header").slick({
   slidesToScroll: 1,
   loop: true,
 });
+
+
+// Xử lý cuộn trang -> stick header
+let lastScrollTop = 0; // Vị trí cuộn trang trước đó
+const header = $('header');
+
+window.addEventListener('scroll', function () {
+    let currentScroll = window.scrollY || document.documentElement.scrollTop;
+    if (currentScroll > 100) {
+        if (currentScroll > lastScrollTop) {
+            // Nếu cuộn xuống, ẩn header
+            header.addClass('hidden');
+            $('.nav-full').removeClass('moveDown');
+        } else {
+            // Nếu cuộn lên, hiện lại header
+            header.removeClass('hidden');
+            $('.nav-full').addClass('moveDown');
+        }
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Đảm bảo không cuộn qua phần đầu trang
+});
+// End header
